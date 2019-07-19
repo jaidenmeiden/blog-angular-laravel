dd15b6d5ae2346deb8643d867aec83fe3d1a7cac
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-19 18:44:37 -0500

    Formulario de registro con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import {appRoutingProviders, routing} from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -19,6 +21,8 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     routing, /*Importamos el modulo routing*/
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     appRoutingProviders, /*Importamos el provider*/
