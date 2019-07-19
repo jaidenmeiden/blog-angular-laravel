6d898677c573b7630902a4e4c0a6b80058d68848
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-19 17:45:37 -0500

    Componente Register

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  public pageTitle: string;
+
+  constructor() {
+    this.pageTitle = 'Registro';
+  }
+
+  ngOnInit() {
+
+  }
+
+}
