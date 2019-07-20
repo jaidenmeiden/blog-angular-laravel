ea60421b2db1b1cda94614d482b52c3a8255752a
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-19 19:03:34 -0500

    Crear servicio de usuarios

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
   public pageTitle: string;
   public user: User;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.pageTitle = 'Registro';
     this.user = new User(1, '', '', '', '', 'ROLE_USER', '', '', '', '', '');
   }
 
   ngOnInit() {
-
+    console.log(this._userService.prueba());
   }
 
   onSubmit(form) {
-
+    console.log(this.user);
+    form.reset();
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  public url: string;
+
+  constructor(
+    public _http: HttpClient /*Cuando se coloca un guión bajo en una propiedad de angular se determina que el final es un servicio*/
+  ) {
+
+  }
+
+  prueba() {
+    return 'Hola mundo';
+  }
+}
