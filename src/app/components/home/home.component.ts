c47220b07283408cd5b3aa89376bf8ae09c538b8
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-23 12:58:25 -0500

    Conseguir posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from '../../models/post';
+import {PostService} from '../../services/post.service';
+import {global} from '../../services/global';
+import {UserService} from '../../services/user.service';
+import {CategoryService} from '../../services/category.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [PostService]
 })
 export class HomeComponent implements OnInit {
 
   public pageTitle: string;
+  public url;
+  public posts: Array<Post>;
 
-  constructor() {
+  constructor(
+    private _postService: PostService
+  ) {
     this.pageTitle = 'Inicio';
+    this.url = global.url;
   }
 
   ngOnInit() {
-
+    this.getPosts();
   }
 
+  getPosts() {
+    this._postService.getPosts(this.token).subscribe(
+      response => {
+        if(response.status == 'success') {
+          this.posts = response.posts;
+        }
+      }, error => {
+        console.error(<any>error);
+      }
+    );
+  }
 }
