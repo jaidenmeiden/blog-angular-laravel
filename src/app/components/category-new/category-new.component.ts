40e7a24b4b6560f2ad74b4d2607ebade296384b5
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-22 20:59:47 -0500

    Vista de creación de categorías

--- src/app/components/category-new/category-new.component.ts
+++ src/app/components/category-new/category-new.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {Category} from '../../models/category';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-category-new',
   templateUrl: './category-new.component.html',
-  styleUrls: ['./category-new.component.css']
+  styleUrls: ['./category-new.component.css'],
+  providers: [UserService]
 })
 export class CategoryNewComponent implements OnInit {
 
-  constructor() { }
+  public pageTitle: string;
+  public category: Category;
+  public identity;
+  public token;
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    this.pageTitle = 'Crear nueva categoría';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.category = new Category(1, '');
+  }
 
   ngOnInit() {
   }
